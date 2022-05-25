32ab115adce9a0791c36711f70d2e0321c8037b4
Jose Alor <josea.alor@gmail.com>
2022-05-25 18:50:57 +0200

    SQL-modifier

--- 3-subconsulta/s6.4.sql
+++ 3-subconsulta/s6.4.sql
@@ -1,5 +1,6 @@
 SELECT * FROM semana_5.usuarios;
 
+INSERT INTO usuarios (dni, tlf, email, nombre, apellidos, edad) VALUES ( "9884038432S", "931247323", "pQlp@mail.com","IFF", "peorp", "92");
 
-SELECT nombre, edad FROM usuarios WHERE (SELECT MAX(edad) FROM usuarios);
+SELECT nombre, edad FROM usuarios WHERE edad = (SELECT MAX(edad) FROM usuarios);
 
--- 4-extra/S6.4.sql
+++ 4-extra/S6.4.sql
@@ -1,7 +1,9 @@
+-- 1.principal sentencia:
+SELECT * FROM usuarios u INNER JOIN direcciones d ON u.id=d.id_usuario WHERE d.pais = (SELECT pais FROM direcciones GROUP BY pais ORDER BY COUNT(*) DESC LIMIT 1) ;
+-- 2.cambiar Indonesia por la subconsulta:
 
-SELECT * FROM usuarios u INNER JOIN direcciones d ON u.id=d.id_usuario WHERE d.pais = "Indonesia";
-
-SELECT d.pais, COUNT(*) FROM direcciones d WHERE MAX(COUNT(*)) GROUP BY d.pais ; 
+SELECT d.pais, MAX(COUNT(*)) FROM direcciones d GROUP BY d.pais; 
 
 -- no funciona sacar d.pais con la condicion MAX(COUNT(*))
-
+-- MEJOR ESTO:
+SELECT pais FROM direcciones GROUP BY pais ORDER BY COUNT(*) DESC LIMIT 1;
